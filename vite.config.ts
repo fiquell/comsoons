d2caa2807ed8281766afaae75ae59b8843fbdeeb
Fiquell <fiquellh@gmail.com>
2023-08-25 20:00:40 +0800

    added aliases folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(function ({ command }) {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+
+  if (command !== "serve") {
+    config.base = "/comsoons/";
+  }
+
+  return config;
 });
