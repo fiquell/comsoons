8b5c7f807ac6ee7933da7629667187f0b8181d58
Fiquell <fiquellh@gmail.com>
2023-08-26 02:10:47 +0800

    added initial navbar components

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
-    <div>
-      <Outlet />
+    <div className="relative">
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 }
