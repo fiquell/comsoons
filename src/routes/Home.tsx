13a010d67afaed63a47fadacc997e421a26e20da
Fiquell <fiquellh@gmail.com>
2023-09-01 11:45:07 +0800

    replace it with form element

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,25 +1,57 @@
 import { ourTeams } from "@/constants";
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [email, setEmail] = useState<string>();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert("* Don't worry, we will not spam you :)");
+    setEmail("");
+  }
+
   return (
     <div>
       <div className="flex min-h-screen flex-col items-center justify-center text-center">
         <p className="mb-12 font-serif text-6xl md:text-7xl lg:text-8xl">
           COMING SOON
         </p>
-        <div className="mb-6">
+        <div className="mb-8">
           <p>We're working on making something amazing.</p>
           <p>
             Subscribe to our newsletter to receive notifications when we arrive.
           </p>
         </div>
-        <Link
+        <form
+          onSubmit={handleSubmit}
+          className="relative w-full md:w-10/12 lg:w-1/2">
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email address"
+            required={true}
+            value={email}
+            className="w-full rounded-3xl px-6 py-4 shadow-md outline outline-2 outline-neutral-900"
+          />
+          <div className="absolute inset-y-0 end-1 grid place-content-center">
+            <button
+              type="submit"
+              className="rounded-3xl bg-neutral-900 px-6 py-3 font-medium text-neutral-100 transition duration-300 ease-in-out hover:scale-x-105 md:px-10">
+              Notify Me
+            </button>
+          </div>
+        </form>
+        {/* <Link
           to="/contacts"
           replace={true}
           className="rounded-md bg-neutral-100 px-8 py-3 font-bold text-neutral-900 shadow-md outline outline-2 outline-neutral-900 transition duration-300 ease-in-out hover:rotate-2 hover:scale-110">
           NOTIFY ME
-        </Link>
+        </Link> */}
       </div>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <p className="mb-24 font-serif text-5xl md:text-6xl lg:text-7xl">
